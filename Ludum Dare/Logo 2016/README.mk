2e9127e8e454cf19b13b20c143a804265e9a3f53
Mike Kasprzak <mike@sykhronics.com>
2016-08-16 19:13:50 -0400

    first commit

---
+++ Ludum Dare/Logo 2016/README.mk
@@ -0,0 +1,10 @@
+# Ludum Dare Font
+The font used is a modified version of NULL by Font Fabric.
+
+http://www.fontfabric.com/null-free-font/
+
+I found at small sizes, the thin accent gaps were tough to read, so I either thickened them or removed them.
+
+The lower case E didn't read that well either, so I used a rotated and flipped (and adjusted) lower case A.
+
+
